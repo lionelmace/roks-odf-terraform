1a293d8729da2f28eddc4e8f21fd1004e60eb75f
Lionel Mace <lionel.mace@gmail.com>
2025-12-12 11:47:35 +0100

    removed container registry

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,47 +13,47 @@ provider "kubernetes" {
   cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
 }
 
-provider "helm" {
-  kubernetes = {
-    host                   = data.ibm_container_cluster_config.cluster_config.host
-    token                  = data.ibm_container_cluster_config.cluster_config.token
-    cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
-  }
-  # IBM Cloud credentials are required to authenticate to the helm repo
-  registries = [{
-    url      = "oci://icr.io/ibm/observe/logs-agent-helm"
-    username = "iamapikey"
-    password = var.ibmcloud_api_key
-  }]
-}
+# provider "helm" {
+#   kubernetes = {
+#     host                   = data.ibm_container_cluster_config.cluster_config.host
+#     token                  = data.ibm_container_cluster_config.cluster_config.token
+#     cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
+#   }
+#   # IBM Cloud credentials are required to authenticate to the helm repo
+#   registries = [{
+#     url      = "oci://icr.io/ibm/observe/logs-agent-helm"
+#     username = "iamapikey"
+#     password = var.ibmcloud_api_key
+#   }]
+# }
 
 
 ########################################################################################################################
 # Container Registry
 ########################################################################################################################
 
-provider "ibm" {
-  alias            = "namespace"
-  ibmcloud_api_key = var.ibmcloud_api_key
-  region           = var.namespace_region
-}
+# provider "ibm" {
+#   alias            = "namespace"
+#   ibmcloud_api_key = var.ibmcloud_api_key
+#   region           = var.namespace_region
+# }
 
-# Data source to retrieve token details
-data "ibm_iam_auth_token" "token_data" {
-}
+# # Data source to retrieve token details
+# data "ibm_iam_auth_token" "token_data" {
+# }
 
-# Data source to account settings
-data "ibm_iam_account_settings" "iam_account_settings" {
-}
+# # Data source to account settings
+# data "ibm_iam_account_settings" "iam_account_settings" {
+# }
 
-provider "restapi" {
-  uri                   = "https:"
-  write_returns_object  = false
-  create_returns_object = false
-  debug                 = false # set to true to show detailed logs, but use carefully as it might print sensitive values.
-  headers = {
-    Account       = data.ibm_iam_account_settings.iam_account_settings.account_id
-    Authorization = data.ibm_iam_auth_token.token_data.iam_access_token
-    Content-Type  = "application/json"
-  }
-}
+# provider "restapi" {
+#   uri                   = "https:"
+#   write_returns_object  = false
+#   create_returns_object = false
+#   debug                 = false # set to true to show detailed logs, but use carefully as it might print sensitive values.
+#   headers = {
+#     Account       = data.ibm_iam_account_settings.iam_account_settings.account_id
+#     Authorization = data.ibm_iam_auth_token.token_data.iam_access_token
+#     Content-Type  = "application/json"
+#   }
+# }
